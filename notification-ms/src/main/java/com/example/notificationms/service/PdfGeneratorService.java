330dec8c448411e8eb10396ebca892cba5a95568
jahangirzadanurlan <cahangirzadenurlan043@gmail.com>
2023-11-25 20:17:56 +0400

    PdfGeneratorService updated

--- notification-ms/src/main/java/com/example/notificationms/service/PdfGeneratorService.java
+++ notification-ms/src/main/java/com/example/notificationms/service/PdfGeneratorService.java
@@ -1,13 +1,12 @@
 package com.example.notificationms.service;
 
 import com.example.commonemail.service.MailSenderService;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayOutputStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.io.ByteArrayOutputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +20,29 @@ public class PdfGeneratorService {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             PdfWriter.getInstance(document, baos);
             document.open();
+            // Başlık
+            Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 24, BaseColor.BLUE);
+            Paragraph title = new Paragraph("Ticket Information", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            document.add(title);
+
+            // Boşluk
+            document.add(Chunk.NEWLINE);
+
+            // Metin
+            Font contentFont = FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, BaseColor.BLACK);
+            Paragraph para = new Paragraph(content, contentFont);
+            para.setAlignment(Element.ALIGN_JUSTIFIED);
+            document.add(para);
+
+            // Boşluk
+            document.add(Chunk.NEWLINE);
+
+            // Resim
+            Image image = Image.getInstance("C:\\Users\\cahan\\airline-management\\common-email\\src\\main\\resources\\travel.jpg");
+            image.setAlignment(Element.ALIGN_CENTER);
+            document.add(image);
+
             document.add(new Paragraph(content));
             document.close();
 
